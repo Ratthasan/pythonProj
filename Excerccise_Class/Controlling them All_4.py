ab6ed6c455b15c0460a336cae0681f9d6edc30ab
Ratthasan <earthhhhh@hotmail.com>
2022-09-04 17:57:30 +0700

    finish

--- Excerccise_Class/Controlling them All_4.py
+++ Excerccise_Class/Controlling them All_4.py
@@ -4,7 +4,10 @@ Hint
 You can create class variables directly in the class without placing them in the constructor. Also, to change the value of the class variable within all objects of the class, access the class variable directly from the class. Here is an example: DriveBot.class_variable = 5.'''
 
 class DriveBot:
-  # Create the class variables!
+    all_disabled = False
+    latitude = -99999
+    longtitude = -99999
+    
 
     def __init__(self, motor_speed = 0, direction = 180, sensor_range = 10):
         self.motor_speed = motor_speed
@@ -26,8 +29,14 @@ robot_1.sensor_range = 10
 robot_2 = DriveBot(35, 75, 25)
 robot_3 = DriveBot(20, 60, 10)
 
-# Change the latitude, longitude, and all_disabled values for all three robots using only three lines of code!
+DriveBot.latitude = 50.0
+DriveBot.longtitude = -50.0
+DriveBot.all_disabled = True 
 
-# print(robot_1.latitude)
-# print(robot_2.longitude)
-# print(robot_3.all_disabled)
+
+
+#print(robot_1.latitude)
+#print(robot_1.longtitude)
+#print(robot_1.all_disabled)
+
+
